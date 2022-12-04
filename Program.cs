0163f242781722ed7ea46cacb815a2997db7e102
Goncharenya <kotmay@mail.ru>
2022-12-04 08:27:13 +0300

    ввод-вывод массивов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] strArray = new string[100];
+
+Console.Write("Задайте кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] myArray = new string[n];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    myArray[i] = Convert.ToString(Console.ReadLine());
+}
+
+string[] array2 = new string[myArray.Length];
+
+void SecondArrayWithIF(string[] myArray, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i].Length <= 3)
+        {
+            array2[count] = myArray[i];
+            count++;
+        }
+    }
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine(array2[i]);
+    }
+}
+
+
+Console.WriteLine(myArray);
+SecondArrayWithIF(myArray, array2);
+ShowArray(array2);
+
+
+
+
